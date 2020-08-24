0d7f24ad1d884d35d2c2a627dcb3e3335c6a4a47
zzamael12114 <zzamael12114@gmail.com>
2020-08-23 22:50:53 -0600

    SG1_PRUEBA

---
+++ Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace LoginBasic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textUser = new System.Windows.Forms.TextBox();
+            this.textPass = new System.Windows.Forms.TextBox();
+            this.buttonIngreso = new System.Windows.Forms.Button();
+            this.buttonRegistro = new System.Windows.Forms.Button();
+            this.buttonSalir = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(254, 114);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Usuario";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(236, 202);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Contraseña";
+            // 
+            // textUser
+            // 
+            this.textUser.Location = new System.Drawing.Point(375, 111);
+            this.textUser.Name = "textUser";
+            this.textUser.Size = new System.Drawing.Size(100, 20);
+            this.textUser.TabIndex = 2;
+            // 
+            // textPass
+            // 
+            this.textPass.Location = new System.Drawing.Point(375, 202);
+            this.textPass.Name = "textPass";
+            this.textPass.PasswordChar = '*';
+            this.textPass.Size = new System.Drawing.Size(100, 20);
+            this.textPass.TabIndex = 3;
+            // 
+            // buttonIngreso
+            // 
+            this.buttonIngreso.Location = new System.Drawing.Point(239, 299);
+            this.buttonIngreso.Name = "buttonIngreso";
+            this.buttonIngreso.Size = new System.Drawing.Size(75, 23);
+            this.buttonIngreso.TabIndex = 4;
+            this.buttonIngreso.Text = "Ingreso";
+            this.buttonIngreso.UseVisualStyleBackColor = true;
+            this.buttonIngreso.Click += new System.EventHandler(this.buttonIngreso_Click);
+            // 
+            // buttonRegistro
+            // 
+            this.buttonRegistro.Location = new System.Drawing.Point(375, 277);
+            this.buttonRegistro.Name = "buttonRegistro";
+            this.buttonRegistro.Size = new System.Drawing.Size(75, 23);
+            this.buttonRegistro.TabIndex = 5;
+            this.buttonRegistro.Text = "Registro";
+            this.buttonRegistro.UseVisualStyleBackColor = true;
+            this.buttonRegistro.Click += new System.EventHandler(this.buttonRegistro_Click);
+            // 
+            // buttonSalir
+            // 
+            this.buttonSalir.Location = new System.Drawing.Point(494, 277);
+            this.buttonSalir.Name = "buttonSalir";
+            this.buttonSalir.Size = new System.Drawing.Size(75, 23);
+            this.buttonSalir.TabIndex = 6;
+            this.buttonSalir.Text = "Salir";
+            this.buttonSalir.UseVisualStyleBackColor = true;
+            this.buttonSalir.Click += new System.EventHandler(this.buttonSalir_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(32, 80);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(172, 185);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(621, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.buttonSalir);
+            this.Controls.Add(this.buttonRegistro);
+            this.Controls.Add(this.buttonIngreso);
+            this.Controls.Add(this.textPass);
+            this.Controls.Add(this.textUser);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textUser;
+        private System.Windows.Forms.TextBox textPass;
+        private System.Windows.Forms.Button buttonIngreso;
+        private System.Windows.Forms.Button buttonRegistro;
+        private System.Windows.Forms.Button buttonSalir;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace LoginBasic
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string password;
+
+        private void buttonRegistro_Click(object sender, EventArgs e)
+        {
+            string user = textUser.Text;
+            string pass = textPass.Text;
+            string url = "C:\\Registro\\" + user + ".txt";
+
+            if (File.Exists(url))
+            {
+                MessageBox.Show("ERROR.  Usuario ya existe!");
+                textUser.Clear();
+                textUser.Clear();
+            }
+            else
+            {
+                File.WriteAllText(url, pass);
+                /*CREA UN NUEVO ARCHIVO CON ESE NOMBRE Y GUARDA DENTRO DEL ARCHIVO EL VAOR DEL SEGUNDO PARAMETRO*/
+                MessageBox.Show("Usuario registrado!");
+                textUser.Clear();
+                textPass.Clear();
+
+            }
+                
+        }
+
+        private void buttonIngreso_Click(object sender, EventArgs e)
+        {
+            string user = textUser.Text;
+            string pass = textPass.Text;
+            string url= "C:\\Registro\\" + user + ".txt";
+
+            if (File.Exists(url))
+            {
+                password = File.ReadAllText(url);
+              //LEE EL TEXTO ALMACENADO DENTRO DEL ARCHIVO 
+
+            if (pass.Equals(password)) //verifica si la contraseña es igual al archivo 
+                {
+                    MessageBox.Show("BIENVENIDO");
+                }
+                else
+                {
+                    MessageBox.Show("CONTRASEÑA INCORRECTA");
+                }
+            }
+            else
+            {
+                MessageBox.Show("USUARIO INCORRECTO");
+                
+            }
+        }
+
+        private void buttonSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
